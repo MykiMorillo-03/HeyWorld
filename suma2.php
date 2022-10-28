3774706443c684287ee967d7baf58284641914fd
MykiMorillo-03 <115846104+MykiMorillo-03@users.noreply.github.com>
2022-10-28 16:58:13 -0500

    Add files via upload

---
+++ suma2.php
@@ -0,0 +1,10 @@
+<?php
+// php04.php - Variables y operaciones aritméticas
+ 
+$n1 = 201;
+$n2 = "35";
+ 
+echo "Los numeros son ", $n1, " y ", $n2, ".<br />";
+echo "Suma: ", $n1+$n2,"<br />";
+
+?>
